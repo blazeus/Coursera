0cfbb2debfdc8ebb4dbadc9e700208dfc65b1e58
Utkarsh Singh <us9044@gmail.com>
2020-07-11 21:39:33 +0530

    restructured

--- Java Specialization/OOP in Java/week 1/casting.java
+++ Java Specialization/OOP in Java/week 1/casting.java
@@ -1,7 +1,7 @@
 public class casting {
     public static void main(String[] args){
         //Implicit casting 
-        // byte > short > int > long > float > double
+        // byte < short < int < long < float < double
         short x =1;
         int y = x + 2;
         System.out.println(y);
