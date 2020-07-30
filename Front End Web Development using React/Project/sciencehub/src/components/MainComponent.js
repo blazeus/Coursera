e9c1a26d65e1c76a4efc1e0d9508a0fb868291dc
Utkarsh Singh <us9044@gmail.com>
2020-07-30 19:39:36 +0530

    Initial Commit

---
+++ Front End Web Development using React/Project/sciencehub/src/components/MainComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return(
+            <div>
+                <h1>
+                    This is a test heading.
+                </h1>
+            </div>
+        );
+    }
+};
+
+export default Main;
